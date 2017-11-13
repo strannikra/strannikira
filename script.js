4b3434d3265f94af50132317c4652cd8d326b9c6
Ivan Yakimov <ivan@debian>
2017-11-13 15:06:09 +0700

    Header with image and text on white background.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let imageList = [
-    "https://pp.userapi.com/c303702/v303702913/244c/nGyuMbG5gq4.jpg",
-    "https://pp.userapi.com/c317331/v317331384/7c6/yKl_Fvx3A6Y.jpg",
-    "https://pp.userapi.com/c1423/u47792213/95383790/x_50ad638a.jpg"
+function getRandomIdx(max) {
+    return (max <= 9 ?
+        Math.floor((Math.random() * 10)+1) % max :
+        0);}
+
+var backgroundImages = [
+    "http://xn--24-6kc3bf4angc2g.xn--p1ai/download/%D0%BC%D0%B0%D1%81%D1%81%D0%BE%D0%B2%D1%8B%D0%B5%20%D0%BC%D0%B5%D1%80%D0%BE%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D1%8F/Semk874vvS4.jpg",
+    "http://xn--24-6kc3bf4angc2g.xn--p1ai/download/%D0%BC%D0%B0%D1%81%D1%81%D0%BE%D0%B2%D1%8B%D0%B5%20%D0%BC%D0%B5%D1%80%D0%BE%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D1%8F/vHE3es6acxc.jpg",
+    "http://xn--24-6kc3bf4angc2g.xn--p1ai/download/%D0%BC%D0%B0%D1%81%D1%81%D0%BE%D0%B2%D1%8B%D0%B5%20%D0%BC%D0%B5%D1%80%D0%BE%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D1%8F/os1mPGriVf0.jpg",
 ];
 
-let index = 1;
+function updateBackground() {
+    let idx = getRandomIdx(backgroundImages.length);
+    let elem = document.querySelector('.header');
+    elem.style.backgroundImage = "url(" + backgroundImages[idx] + ")";
+}
 
-let galery = document.getElementById('galery');
-
-galery.onclick = function (event) {
-    if (index < imageList.length-1) 
-        index++; 
-    else 
-        index = 0;
-    galery.src = imageList[index];
-};
-
+updateBackground();
